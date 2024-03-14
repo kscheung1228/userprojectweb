4b39c6d710db567b765ff94102fec004a8783b97
kscheung1228 <kscheung1228@gmail.com>
2024-03-14 21:49:11 +0800

    before adding cookie

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import LoginForm from './components/LoginForm';
 import Dashboard from './components/DashBoard';
 import PasswordResetForm from './components/PasswordReset'
 import VerifyEmail from './components/VerifyEmail';
+import PasswordResetConfirmForm from './components/PasswordResetConfirm';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Route exact path="/dj-rest-auth/login" element={<LoginForm />} />
         <Route exact path="/dashboard" element={<Dashboard />} />
         <Route exact path="/dj-rest-auth/password/reset" element={<PasswordResetForm />} />
+        <Route path="password-reset/confirm/:Uid/:Token/" element={<PasswordResetConfirmForm />} />
         <Route path="/dj-rest-auth/registration/account-confirm-email/:Key" element={<VerifyEmail />} />
       </Routes>
     </Router>
---
+++ src/components/PasswordResetConfirm.js
@@ -0,0 +1,120 @@
+import React, { useState , useEffect } from 'react';
+import { apinointercept } from './api';
+import { useLocation, useParams } from 'react-router-dom';
+
+
+
+function PasswordResetConfirmForm() {
+    const [message, setMessage] = useState('');
+    const [error, setError] = useState();
+
+    const [formData, setFormData] = useState({
+      uid: '',
+      token: '',
+      new_password1: '',
+      new_password2: '',
+    });
+
+
+    const { pathname, search, params } = useCurrentURL();
+    console.log(`Current path is ${pathname} with search ${search} and params`, params);
+    
+    useEffect(() => {
+        if (typeof (params.Uid) !== 'undefined') {
+            formData.uid = params.Uid
+            formData.token = params.Token
+            // setFormData({"uid":params.Uid,})
+            // setFormData({"token":params.Token})
+            console.log ("usereffect",formData)
+            
+        }
+      }, [params.Uid,params.Token,formData]);
+
+    // Handle form input changes
+    const handleChange = (e) => {
+        // e.target.name = e.target.value
+      setFormData({ ...formData, [e.target.name]: e.target.value });
+      console.log ("handlechange",formData, typeof(formData))
+    };
+
+
+    // Handle form submission
+    const handleSubmit = async (e) => {
+      e.preventDefault();
+        console.log("formdata",formData)
+      try {
+        // Make a POST request to the login endpoint of your API to obtain the JWT token
+        const response = await apinointercept.post('/password-reset/confirm/', formData);
+        setMessage(response.data);
+        setError('');
+        // Handle the response, e.g., store the JWT token in local storage or state
+        // localStorage.setItem('token', response.data.token); // Store the JWT token in local storage
+        // Redirect to the dashboard or perform any other necessary action
+ 
+      } catch (error) {
+        // Handle the error, e.g., display an error message
+        console.error("loginerror",error);
+        setMessage('');
+        setError(JSON.stringify(error.response.data));
+      }
+    };
+  
+    // Render the login form
+    return (
+      <form onSubmit={handleSubmit}>
+        <input type="password" name="new_password1" value={formData.username} onChange={handleChange} placeholder="Password" />
+        <input type="password" name="new_password2" value={formData.password} onChange={handleChange} placeholder="Repeat Password" />
+        <button type="submit">Reset Password</button>
+        <div>
+                {error && <p>{JSON.stringify(error)}</p>}
+                {message && <p>{JSON.stringify(message)}</p>}
+            </div>
+      </form>
+      
+    );
+  }
+  
+  export default PasswordResetConfirmForm;
+
+
+
+function useCurrentURL() {
+    const location = useLocation();
+    const params = useParams();
+  
+    return {
+      pathname: location.pathname,
+      search: location.search,
+      params,
+    };
+  }
+
+// export function VerifyEmail() {
+
+    // setFormData({Key:params})
+  
+      
+    // try {
+        
+        // Make a POST request to the login endpoint of your API to obtain the JWT token
+        // const response = api.post('/dj-rest-auth/registration/verify-email/', formData);
+        // setMessage(response.data);
+
+        // Handle the response, e.g., store the JWT token in local storage or state
+
+    //   } catch (error) {
+        // Handle the error, e.g., display an error message
+        // console.error("loginerror",error);
+        // setMessage('');
+        // setError(JSON.stringify(error.response.data));
+    //   }
+
+    //   return (
+        //   <div>
+        //           {error && <p>{JSON.stringify(error)}</p>}
+        //           {message && <p>{JSON.stringify(message)}</p>}
+        //       </div>
+    //   );
+//   }
+  
+//   export default VerifyEmail;
